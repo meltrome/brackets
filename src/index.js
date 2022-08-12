424cc16173f8c304a4f5702b7e28bdabf7d60120
Maksim <ilyshev@mail.ru>
2022-08-12 13:09:26 +0300

    feat: my code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  let open = [];
+  let pair = {};
+  let stack = [];
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+
+    let bracketKey = bracketsConfig[i][1];
+    let bracketValue = bracketsConfig[i][0];
+
+    open.push(bracketsConfig[i][0]);
+    pair[bracketKey] = bracketValue;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+
+    let curSym = str[i];
+
+    if (stack.length && stack[stack.length - 1] === pair[curSym]);
+
+    if (open.includes(curSym)) {
+       stack.push(curSym);
+    } else {
+      if (stack.length === 0){
+        return false;
+      }
+    
+      let topElement = stack[stack.length - 1];
+
+      if (pair[curSym] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }  
+    }
+  }
+  
+  return stack.length == 0;
+
+  
+
+};
